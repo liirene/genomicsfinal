44c30128acff68ed1d109acf7f991a30d40b2b63
Irene Li <liirene@stanford.edu>
2017-03-15 18:59:10 -0700

    testing commit

--- filter_calls.py
+++ filter_calls.py
@@ -12,3 +12,57 @@ the ones that were taken out to be taken out lel
 
 # Something here to decide whether running as a module or as a main system file
 import sys
+import re
+
+# Currently call looks like: python3 filtercalls.py input.vcf output.vcf
+
+# define variables
+inputvcf = sys.argv[1]
+
+# empty dictionary for calls and their associated attributes
+calls = {}
+
+# filter out all calls that don't have a call quality associated
+with open(inputvcf, mode = "rt") as in_vcf:
+    for line in in_vcf:
+        if not line.startswith("#"):
+            chrom, pos, ntid, ref, alt, qual, filt, info, line_format, \
+            var_match = line.rstrip(
+                "\t\n").split("\t")
+
+            if not qual == ".":
+                info_dict = {}
+                clinvar_query = []
+
+                # pull out query values for omim
+
+                # Pull out query values for clinvar
+                num = re.findall("(?<=chr)\w", chrom)[0] + "[chr]"
+                gene = re.findall("\(([^\)]+)\)", var_match)
+                hgnv = re.findall("c\.[^\,\:\(\s]+", var_match)
+                refseq = re.findall("N\w?_[^\(]+", var_match)
+
+                query_list = [gene, hgnv, refseq]
+
+                # test if any are none
+
+                clinvar_query.append(num)
+                for item in query_list:
+                    if not len(item) == 0:
+                        clinvar_query.append(item[0])
+
+                # Archive the information for use later... make into lists
+                info = info.split(";")
+                line_format = line_format.split(":")
+                var_match = var_match.split(":")
+
+                clinvar_query = " AND ".join(clinvar_query)
+
+                # Create dictionary with important values for our analyses
+                for i, field in enumerate(line_format):
+                    info_dict[field] = var_match[i]
+
+                print(clinvar_query,"\n")
+
+                #calls.update(
+                  #  {(chrom, pos): [ref, alt, qual, info_dict, clinvar_query]})
