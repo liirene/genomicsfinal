dd304f63e3ea7b6dfdbd195c4d1984c7f452c039
Irene Li <liirene@stanford.edu>
2017-03-18 15:43:45 -0700

    Fixing R pull and including line parsing structures

--- R_pull.py
+++ R_pull.py
@@ -1,15 +1,47 @@
-def pathway_parse(path_to_script, args):
-    import subprocess  # for running r through later
+import subprocess
+from typing import Tuple, List, Dict
+def pathway_parse(path_to_script):
     command = 'C:/Program Files/R/R-3.3.2/bin/Rscript.exe'
-    path_to_script = 'pathwayscript.R'
     args = ['clean_data.csv']
     cmd = [command, path_to_script] + args
 
     x = subprocess.check_output(cmd, universal_newlines=True)
+    return x
 
-###### PLOT MAKING
- # Read plot name from the pathway?
+# Upreg is before downreg
+def split_r_output(output_lines: List[str]) -> Tuple[List[str], List[str]]:
+    upreg = []
+    downreg = []
+    is_upreg = True
+    for line in output_lines:
+        if line == "":
+            is_upreg = False
+        elif is_upreg is True:
+            upreg.append(line)
+        else:
+            downreg.append(line)
+    return (upreg, downreg)
 
+def parse_gene_list(gene_list: List[str]) -> Dict[str, List[str]]:
+    pathways = {}
+    current_pathway = ""
+    for line in gene_list:
+        if line.startswith("# "):
+            line = line.lstrip("# ")
+            pathways[line] = []
+            current_pathway = line
+        else:
+            pathways[current_pathway] += [line.rstrip("\n\t ")]
+    return pathways
+
+
+output = pathway_parse('pathwayscript.R')
+#print(output)
+output_lines = output.split("\n")
+output_lines = list(filter(lambda line: line.startswith("[1]") is False, output_lines))
+upreg, downreg = split_r_output(output_lines)
+upreg_pathways = parse_gene_list(upreg)
+downreg_pathways = parse_gene_list(downreg)
 
 # Do this for each group of pathways that you're interested in (1 figure for
 # each group of pathways)
